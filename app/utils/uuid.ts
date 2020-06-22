233cd7deb05ea79cffb2141ffb0b7489aaa2cb42
lenconda <i@lenconda.top>
2020-06-23 00:37:52 +0800

    feat: 更新UUID生成函数

--- app/utils/uuid.ts
+++ app/utils/uuid.ts
@@ -1,3 +1,6 @@
-import uuid from 'uuid/v4';
+import uuidv4 from 'uuid/v4';
+import uuidv5 from 'uuid/v5';
 
-export const generateUuid = () => new Buffer(uuid()).toString('base64');
+export const uuid = (seed: string = Date.now().toString()) => {
+  return Buffer.from(uuidv5(seed, uuidv4())).toString('base64').slice(-9, -1);
+};
